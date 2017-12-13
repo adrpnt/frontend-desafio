a6a2314b6a54786e4104de30697b550dd83da1da
Adriano Vasconcelos <adriano.vasconcelos@secaoweb.com.br>
2017-12-13 14:51:50 -0300

    add routes file

---
+++ src/routes/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route, Redirect, Switch } from 'react-router-dom';
+
+import Home from '../pages/home';
+import Favorites from '../pages/favorites';
+
+export default props => (
+  <Switch>
+    <Route exact path='/' component={ Home } />
+    <Route path='/favorites' component={ Favorites } />
+
+    <Redirect from='*' to='/' />
+  </Switch>
+)

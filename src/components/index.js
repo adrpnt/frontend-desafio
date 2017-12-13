1bae035075085a058f6c3c25ecd13bc6aa212558
Adriano Vasconcelos <adriano.vasconcelos@secaoweb.com.br>
2017-12-13 14:50:05 -0300

    reorganize structure and files

---
+++ src/components/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Toolbar from './toolbar';
+
+import 'bulma/css/bulma.css';
+
+import Routes from '../routes';
+
+class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <Toolbar />
+        <Routes />
+      </div>
+    );
+  }
+}
+
+export default App;
